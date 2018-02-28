58c720022b44712d533709776124ab58994eea52
Rainbow007 <rainbow007@Rainbow007s-MacBook-Pro.local>
2018-02-28 23:10:20 +0700

    updated 1.0

---
+++ app/src/main/java/com/rainbow007/xukashop/View/ManHinhChao/ManHinhChaoActivity.java
@@ -0,0 +1,35 @@
+package com.rainbow007.xukashop.View.ManHinhChao;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.rainbow007.xukashop.R;
+import com.rainbow007.xukashop.View.TrangChu.TrangChuActivity;
+
+/**
+ * Created by rainbow007 on 2/21/18.
+ */
+
+public class ManHinhChaoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manhinhchao_layout);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (Exception e) {
+
+                } finally {
+                    Intent intent = new Intent(ManHinhChaoActivity.this, TrangChuActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+        thread.start();
+    }
+}
---
+++ app/src/main/java/com/rainbow007/xukashop/View/TrangChu/TrangChuActivity.java
@@ -0,0 +1,18 @@
+package com.rainbow007.xukashop.View.TrangChu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.rainbow007.xukashop.R;
+
+/**
+ * Created by rainbow007 on 2/21/18.
+ */
+
+public class TrangChuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trangchu_layout );
+    }
+}
