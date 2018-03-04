9f170b8f39e64ab76709d50bb1c9f6a8efe2cdfc
Rainbow007 <rainbow007@Rainbow007s-MacBook-Pro.local>
2018-03-04 22:20:40 +0700

    Fragment

--- app/src/main/java/com/rainbow007/xukashop/View/TrangChu/Fragment/FragmentDienTu.java
+++ app/src/main/java/com/rainbow007/xukashop/View/TrangChu/Fragment/FragmentDienTu.java
@@ -1,8 +1,24 @@
 package com.rainbow007.xukashop.View.TrangChu.Fragment;
 
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rainbow007.xukashop.R;
+
 /**
  * Created by rainbow007 on 3/4/18.
  */
 
-public class FragmentDienTu {
+public class FragmentDienTu extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_dientu, container, false);
+        return view;    }
 }
--- app/src/main/java/com/rainbow007/xukashop/View/TrangChu/Fragment/FragmentNhaCua.java
+++ app/src/main/java/com/rainbow007/xukashop/View/TrangChu/Fragment/FragmentNhaCua.java
@@ -1,8 +1,23 @@
 package com.rainbow007.xukashop.View.TrangChu.Fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rainbow007.xukashop.R;
+
 /**
  * Created by rainbow007 on 3/4/18.
  */
 
-public class FragmentNhaCua {
+public class FragmentNhaCua extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_nhacua, container, false);
+        return view;    }
 }
