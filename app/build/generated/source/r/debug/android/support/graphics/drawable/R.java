89a1be10400689e62f2ecd41a834ffe9fae03aab
Thang <emlathang007@gmail.com>
2018-04-27 17:27:21 +0700

    v22

--- app/build/generated/source/r/debug/android/support/graphics/drawable/R.java
+++ app/build/generated/source/r/debug/android/support/graphics/drawable/R.java
@@ -83,24 +83,24 @@ public final class R {
         public static final int notification_background = 0x7f08009d;
         public static final int notification_main_column = 0x7f08009e;
         public static final int notification_main_column_container = 0x7f08009f;
-        public static final int right_icon = 0x7f0800ad;
-        public static final int right_side = 0x7f0800ae;
-        public static final int tag_transition_group = 0x7f0800db;
-        public static final int text = 0x7f0800dc;
-        public static final int text2 = 0x7f0800dd;
-        public static final int time = 0x7f0800e3;
-        public static final int title = 0x7f0800e4;
+        public static final int right_icon = 0x7f0800ae;
+        public static final int right_side = 0x7f0800af;
+        public static final int tag_transition_group = 0x7f0800dc;
+        public static final int text = 0x7f0800dd;
+        public static final int text2 = 0x7f0800de;
+        public static final int time = 0x7f0800e4;
+        public static final int title = 0x7f0800e5;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f09000a;
     }
     public static final class layout {
-        public static final int notification_action = 0x7f0a0041;
-        public static final int notification_action_tombstone = 0x7f0a0042;
-        public static final int notification_template_custom_big = 0x7f0a0049;
-        public static final int notification_template_icon_group = 0x7f0a004a;
-        public static final int notification_template_part_chronometer = 0x7f0a004e;
-        public static final int notification_template_part_time = 0x7f0a004f;
+        public static final int notification_action = 0x7f0a0042;
+        public static final int notification_action_tombstone = 0x7f0a0043;
+        public static final int notification_template_custom_big = 0x7f0a004a;
+        public static final int notification_template_icon_group = 0x7f0a004b;
+        public static final int notification_template_part_chronometer = 0x7f0a004f;
+        public static final int notification_template_part_time = 0x7f0a0050;
     }
     public static final class string {
         public static final int status_bar_notification_info_overflow = 0x7f0d0056;
--- app/src/main/java/com/rainbow007/xukashop/View/TrangChu/Fragment/FragmentChuongTrinhKM.java
+++ app/src/main/java/com/rainbow007/xukashop/View/TrangChu/Fragment/FragmentChuongTrinhKM.java
@@ -3,11 +3,18 @@ package com.rainbow007.xukashop.View.TrangChu.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.rainbow007.xukashop.CustomAdapter.AdapterCTKM;
 import com.rainbow007.xukashop.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by rainbow007 on 3/4/18.
@@ -15,11 +22,30 @@ import com.rainbow007.xukashop.R;
 
 public class FragmentChuongTrinhKM extends Fragment {
 
+    RecyclerView recyclerView;
+    AdapterCTKM adapterCTKM;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.layout_chuongtrinhkm, container, false);
+
+        recyclerView = view.findViewById(R.id.recycleCTKM);
+        List<String> data = new ArrayList<>();
+
+        for (int i = 0; i < 50; i++) {
+            String ten = "Chuong trinh khuyen mai " + i;
+            data.add(ten);
+        }
+
+        //khoi tao cac instance
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false);
+        adapterCTKM = new AdapterCTKM(getActivity(), data);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapterCTKM);
+        adapterCTKM.notifyDataSetChanged();
         return view;
     }
 }
