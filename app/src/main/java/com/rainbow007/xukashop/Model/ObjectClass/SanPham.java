cb87b124ce5582763d9810dbc83ccbb2a8ba1a5d
Thang <emlathang007@gmail.com>
2018-04-30 19:06:27 +0700

    v24

---
+++ app/src/main/java/com/rainbow007/xukashop/Model/ObjectClass/SanPham.java
@@ -0,0 +1,95 @@
+package com.rainbow007.xukashop.Model.ObjectClass;
+
+public class SanPham {
+
+    private int masp;
+    private int giaNhap;
+    private int giaBan;
+    private int soluong;
+    private String tenSp;
+    private String anhNho;
+    private String anhLon;
+    private String thongTin;
+    private String maLoaiSp;
+    private String maLoaiThuongHieu;
+
+    public int getMasp() {
+        return masp;
+    }
+
+    public void setMasp(int masp) {
+        this.masp = masp;
+    }
+
+    public int getGiaNhap() {
+        return giaNhap;
+    }
+
+    public void setGiaNhap(int giaNhap) {
+        this.giaNhap = giaNhap;
+    }
+
+    public int getGiaBan() {
+        return giaBan;
+    }
+
+    public void setGiaBan(int giaBan) {
+        this.giaBan = giaBan;
+    }
+
+    public int getSoluong() {
+        return soluong;
+    }
+
+    public void setSoluong(int soluong) {
+        this.soluong = soluong;
+    }
+
+    public String getTenSp() {
+        return tenSp;
+    }
+
+    public void setTenSp(String tenSp) {
+        this.tenSp = tenSp;
+    }
+
+    public String getAnhNho() {
+        return anhNho;
+    }
+
+    public void setAnhNho(String anhNho) {
+        this.anhNho = anhNho;
+    }
+
+    public String getAnhLon() {
+        return anhLon;
+    }
+
+    public void setAnhLon(String anhLon) {
+        this.anhLon = anhLon;
+    }
+
+    public String getThongTin() {
+        return thongTin;
+    }
+
+    public void setThongTin(String thongTin) {
+        this.thongTin = thongTin;
+    }
+
+    public String getMaLoaiSp() {
+        return maLoaiSp;
+    }
+
+    public void setMaLoaiSp(String maLoaiSp) {
+        this.maLoaiSp = maLoaiSp;
+    }
+
+    public String getMaLoaiThuongHieu() {
+        return maLoaiThuongHieu;
+    }
+
+    public void setMaLoaiThuongHieu(String maLoaiThuongHieu) {
+        this.maLoaiThuongHieu = maLoaiThuongHieu;
+    }
+}
---
+++ app/src/main/java/com/rainbow007/xukashop/Model/ObjectClass/ThuongHieu.java
@@ -0,0 +1,40 @@
+package com.rainbow007.xukashop.Model.ObjectClass;
+
+public class ThuongHieu {
+    private String hinhThuongHieu;
+    private String maThuongHieu;
+    private String tenThuongHieu;
+    private int luotMua;
+
+    public String getHinhThuongHieu() {
+        return hinhThuongHieu;
+    }
+
+    public void setHinhThuongHieu(String hinhThuongHieu) {
+        this.hinhThuongHieu = hinhThuongHieu;
+    }
+
+    public String getMaThuongHieu() {
+        return maThuongHieu;
+    }
+
+    public void setMaThuongHieu(String maThuongHieu) {
+        this.maThuongHieu = maThuongHieu;
+    }
+
+    public String getTenThuongHieu() {
+        return tenThuongHieu;
+    }
+
+    public void setTenThuongHieu(String tenThuongHieu) {
+        this.tenThuongHieu = tenThuongHieu;
+    }
+
+    public int getLuotMua() {
+        return luotMua;
+    }
+
+    public void setLuotMua(int luotMua) {
+        this.luotMua = luotMua;
+    }
+}
