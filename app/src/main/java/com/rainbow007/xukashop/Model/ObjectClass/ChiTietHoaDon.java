69849d6d96e4dded395b8a14b4173e3d99c970ef
Rainbow007 <quocthang.it247@gmail.com>
2018-05-16 10:52:15 +0700

    v45 10p

---
+++ app/src/main/java/com/rainbow007/xukashop/Model/ObjectClass/ChiTietHoaDon.java
@@ -0,0 +1,32 @@
+package com.rainbow007.xukashop.Model.ObjectClass;
+
+public class ChiTietHoaDon {
+
+    private int Soluong;
+    private String MaDonHang;
+    private String MaSp;
+
+    public int getSoluong() {
+        return Soluong;
+    }
+
+    public void setSoluong(int soluong) {
+        Soluong = soluong;
+    }
+
+    public String getMaDonHang() {
+        return MaDonHang;
+    }
+
+    public void setMaDonHang(String maDonHang) {
+        MaDonHang = maDonHang;
+    }
+
+    public String getMaSp() {
+        return MaSp;
+    }
+
+    public void setMaSp(String maSp) {
+        MaSp = maSp;
+    }
+}
---
+++ app/src/main/java/com/rainbow007/xukashop/Model/ObjectClass/HoaDon.java
@@ -0,0 +1,97 @@
+package com.rainbow007.xukashop.Model.ObjectClass;
+
+import java.util.List;
+
+public class HoaDon {
+
+    private String MaDonHang;
+    private String TrangThai;
+    private String DiaChiGiaoHang;
+    private String NgayDat;
+    private String NgayGiao;
+    private String TenKH;
+    private String Sdt;
+    private String MaCK;
+    private int ChuyenKhoan;
+    private List<ChiTietHoaDon> chiTietHoaDonList;
+
+    public String getMaDonHang() {
+        return MaDonHang;
+    }
+
+    public void setMaDonHang(String maDonHang) {
+        MaDonHang = maDonHang;
+    }
+
+    public String getTrangThai() {
+        return TrangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        TrangThai = trangThai;
+    }
+
+    public String getDiaChiGiaoHang() {
+        return DiaChiGiaoHang;
+    }
+
+    public void setDiaChiGiaoHang(String diaChiGiaoHang) {
+        DiaChiGiaoHang = diaChiGiaoHang;
+    }
+
+    public String getNgayDat() {
+        return NgayDat;
+    }
+
+    public void setNgayDat(String ngayDat) {
+        NgayDat = ngayDat;
+    }
+
+    public String getNgayGiao() {
+        return NgayGiao;
+    }
+
+    public void setNgayGiao(String ngayGiao) {
+        NgayGiao = ngayGiao;
+    }
+
+    public String getTenKH() {
+        return TenKH;
+    }
+
+    public void setTenKH(String tenKH) {
+        TenKH = tenKH;
+    }
+
+    public String getSdt() {
+        return Sdt;
+    }
+
+    public void setSdt(String sdt) {
+        Sdt = sdt;
+    }
+
+    public String getMaCK() {
+        return MaCK;
+    }
+
+    public void setMaCK(String maCK) {
+        MaCK = maCK;
+    }
+
+    public int getChuyenKhoan() {
+        return ChuyenKhoan;
+    }
+
+    public void setChuyenKhoan(int chuyenKhoan) {
+        ChuyenKhoan = chuyenKhoan;
+    }
+
+    public List<ChiTietHoaDon> getChiTietHoaDonList() {
+        return chiTietHoaDonList;
+    }
+
+    public void setChiTietHoaDonList(List<ChiTietHoaDon> chiTietHoaDonList) {
+        this.chiTietHoaDonList = chiTietHoaDonList;
+    }
+}
